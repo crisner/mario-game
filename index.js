26257e7821bae7414d0bfc40c2f37f1e3df92453
Renisha Christie <christie.reni@gmail.com>
2022-05-05 20:23:16 +0530

    Add images

--- index.js
+++ index.js
@@ -1,140 +1,215 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-console.log(ctx)
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+console.log(ctx);
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight - 4
-const gravity = 1.5
+canvas.width = 1240;
+canvas.height = 576;
+const gravity = 1.5;
+let rightOffset = 0;
+
+function createImage(imageSrc) {
+  const image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+
+const platformImg = createImage("http://127.0.0.1:5500/images/platform.png");
+class GenericObjects {
+  constructor({ x, y, image }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.image = image;
+    this.width = this.image.width;
+    this.height = this.image.height;
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.image.width,
+      this.image.height
+    );
+  }
+}
 
 class Platform {
-    constructor({x, y}) {
-        this.position = {
-            x,
-            y
-        }
-        this.width = 200
-        this.height = 20
-    }
+  constructor({ x, y, image }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.image = image;
+    this.width = this.image.width;
+    this.height = this.image.height;
+  }
 
-    draw() {
-        ctx.fillStyle = 'blue'
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.image.width,
+      this.image.height
+    );
+  }
 }
 class Player {
-    constructor() {
-        this.position = {
-            x: 100,
-            y: 100
-        }
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
-        this.width = 20
-        this.height = 20
-    }
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.width = 20;
+    this.height = 20;
+  }
 
-    draw() {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
 
-    update() {
-        this.draw()
-        platforms.forEach(platform => {
-            platform.draw()
-        })
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
+  update() {
+    genericObjects.forEach((genericObject) => {
+      genericObject.draw();
+    });
+    this.draw();
+    platforms.forEach((platform) => {
+      platform.draw();
+    });
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
 
-        if(this.position.y + this.height + this.velocity.y <= canvas.height) {
-            this.velocity.y += gravity
-        } else {
-            this.velocity.y = 0
-        }
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
     }
+  }
 }
 
-const player = new Player()
-const platforms = [new Platform({x:200, y:100}), new Platform({x:600, y:200})]
-
+const player = new Player();
+const platforms = [
+  new Platform({ x: -1, y: 470, image: platformImg }),
+  new Platform({ x: platformImg.width - 3, y: 470, image: platformImg }),
+  new Platform({ x: platformImg.width * 2 + 100, y: 470, image: platformImg }),
+  new Platform({ x: platformImg.width * 3 + 300, y: 470, image: platformImg }),
+  new Platform({ x: platformImg.width * 5, y: 470, image: platformImg }),
+  new Platform({ x: platformImg.width * 6 - 2, y: 470, image: platformImg }),
+];
+const genericObjects = [
+  new GenericObjects({
+    x: -1,
+    y: -1,
+    image: createImage("http://127.0.0.1:5500/images/background.png"),
+  }),
+  new GenericObjects({
+    x: 0,
+    y: 0,
+    image: createImage("http://127.0.0.1:5500/images/hills.png"),
+  }),
+];
 
 const keys = {
-    right: {
-        pressed: false
-    },
-    left: {
-        pressed: false
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+    genericObjects.forEach((genericObject) => {
+      if (keys.right.pressed) {
+        genericObject.position.x -= 2;
+      } else if (keys.left.pressed) {
+        genericObject.position.x += 2;
+      }
+    });
+    platforms.forEach((platform) => {
+      if (keys.right.pressed) {
+        rightOffset += 5;
+        platform.position.x -= 5;
+      } else if (keys.left.pressed) {
+        rightOffset -= 5;
+        platform.position.x += 5;
+      }
+    });
+  }
+
+  // Platform collision detection
+  platforms.forEach((platform) => {
+    if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y &&
+      player.position.x + player.width >= platform.position.x &&
+      player.position.x <= platform.position.x + platform.width
+    ) {
+      player.velocity.y = 0;
     }
+  });
+
+  if (rightOffset > 6000) {
+    console.log("You win!");
+  }
+  if (player.position.y > canvas.height) {
+    console.log("You lose");
+  }
 }
 
-function animate () {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    if(keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5
-    } else if(keys.left.pressed && player.position.x > 100) {
-        player.velocity.x = -5
-    } else {
-        player.velocity.x = 0
-        platforms.forEach(platform => {
-            if(keys.right.pressed) {
-                platform.position.x -= 5
-            } else if(keys.left.pressed) {
-                platform.position.x += 5
-            }
-        })
-    }
-    
-    // Platform collision detection
-    platforms.forEach(platform => {
-        if(player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
-            player.velocity.y = 0
-        }
-    })
-}
+animate();
 
-animate()
+window.addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = true;
+      break;
+    case 68:
+      console.log("right");
+      keys.right.pressed = true;
+      break;
+    case 87:
+      console.log("top");
+      player.velocity.y -= 20;
+      break;
+    case 83:
+      console.log("down");
+      break;
+  }
+});
 
-window.addEventListener('keydown', ({keyCode}) => {
-    switch(keyCode) {
-        case 65:
-            console.log('left')
-            keys.left.pressed = true
-            break
-        case 68:
-            console.log('right')
-            keys.right.pressed = true
-            break
-        case 87:
-            console.log('top')
-            player.velocity.y -= 20
-            break
-        case 83:
-            console.log('down')
-            break
-        
-    }
-})
-
-window.addEventListener('keyup', ({keyCode}) => {
-    switch(keyCode) {
-        case 65:
-            console.log('left')
-            keys.left.pressed = false
-            break
-        case 68:
-            console.log('right')
-            keys.right.pressed = false
-            break
-        case 87:
-            console.log('top')
-            break
-        case 83:
-            console.log('down')
-            break
-        
-    }
-})
+window.addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+    case 68:
+      console.log("right");
+      keys.right.pressed = false;
+      break;
+    case 87:
+      console.log("top");
+      break;
+    case 83:
+      console.log("down");
+      break;
+  }
+});
